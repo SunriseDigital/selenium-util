5f09415f0541375ad95b98c1fe54254e7ffdb7f5
Masamoto Miyata <miyata@sincere-co.com>
2012-07-24 14:22:59 +0900

    waitForFindElementsを追加 waitForCountElementsを追加 waitForDisappearElementを追加 assertNotExistsElementを追加 sleep_interval, wait_countの直書きをやめました SearchContextを返していたメソッドの返り値はWebElementで問題ないので変更

--- jp/gomo/selenium/SeleniumTestCase.java
+++ jp/gomo/selenium/SeleniumTestCase.java
@@ -1,15 +1,22 @@
 package jp.gomo.selenium;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 
 import junit.framework.TestCase;
 
 public class SeleniumTestCase extends TestCase {
+	
+	private int sleep_interval = 200;
+	
+	private int default_wait_count = 10;
 	
 	protected String getFileAbsolutePath(String path)
 	{
@@ -19,11 +26,11 @@ public class SeleniumTestCase extends TestCase {
 	
 	protected WebElement waitForDisplay(WebElement element) throws InterruptedException
 	{
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < default_wait_count; i++)
 		{
 			if(!element.isDisplayed())
 			{
-				Thread.sleep(200);
+				Thread.sleep(sleep_interval);
 			}
 			else
 			{
@@ -36,22 +43,22 @@ public class SeleniumTestCase extends TestCase {
 			fail(element+" is not displayed.");
 		}
 		
-		Thread.sleep(200);
+		Thread.sleep(sleep_interval);
 		
 		return element;
 	}
 	
-	protected SearchContext waitForFindElement(String message, SearchContext element, By selector) throws InterruptedException 
+	protected WebElement waitForFindElement(String message, SearchContext element, By selector) throws InterruptedException 
 	{
-		return waitForFindElement(message, element, selector, 10);
+		return waitForFindElement(message, element, selector, default_wait_count);
 	}
 	
-	protected SearchContext waitForFindElement(SearchContext element, By selector) throws InterruptedException 
+	protected WebElement waitForFindElement(SearchContext element, By selector) throws InterruptedException 
 	{
-		return waitForFindElement("Not found element for "+selector, element, selector, 10);
+		return waitForFindElement("Not found element for "+selector, element, selector, default_wait_count);
 	}
 
-	protected SearchContext waitForFindElement(String message, SearchContext element, By selector, int waitCount) throws InterruptedException 
+	protected WebElement waitForFindElement(String message, SearchContext element, By selector, int waitCount) throws InterruptedException 
 	{
 		WebElement target = null;
 		
@@ -63,7 +70,7 @@ public class SeleniumTestCase extends TestCase {
 				break;
 				
 			} catch (NoSuchElementException e) {
-				Thread.sleep(200);
+				Thread.sleep(sleep_interval);
 			}
 		}
 		
@@ -75,4 +82,103 @@ public class SeleniumTestCase extends TestCase {
 		return target;
 	}
 	
+	protected List<WebElement> waitForFindElements(SearchContext element, By selector) throws InterruptedException 
+	{
+		return waitForFindElements("Not found elements for "+selector, element, selector, default_wait_count);
+	}
+	
+	protected List<WebElement> waitForFindElements(String message, SearchContext element, By selector, int waitCount) throws InterruptedException 
+	{
+		for (int i = 0; i < waitCount; i++)
+		{
+			List<WebElement> list = element.findElements(selector);
+			if(list.size() != 0)
+			{
+				return list;
+			}
+			
+			Thread.sleep(sleep_interval);
+		}
+		
+		fail(message);
+		
+		return null;
+	}
+	
+	protected List<WebElement> waitForCountElements(SearchContext element, By selector, int expectedCount) throws InterruptedException 
+	{
+		return waitForCountElements("Not found elements for "+selector, element, selector, expectedCount, default_wait_count);
+	}
+	
+	protected List<WebElement> waitForCountElements(String message, SearchContext element, By selector, int expectedCount, int waitCount) throws InterruptedException 
+	{
+		for (int i = 0; i < waitCount; i++)
+		{
+			List<WebElement> list = element.findElements(selector);
+			if(list.size() == expectedCount)
+			{
+				return list;
+			}
+			
+			Thread.sleep(sleep_interval);
+		}
+		
+		fail(message);
+		
+		return null;
+	}
+	
+	protected void waitForDisappearElement(WebElement element) throws InterruptedException 
+	{
+		waitForDisappearElement(element+" is still exists.", element, default_wait_count);
+	}
+	
+	protected void waitForDisappearElement(String message, WebElement element, int waitCount) throws InterruptedException 
+	{
+		for (int i = 0; i < waitCount; i++)
+		{
+			try {
+				
+				if(!element.isDisplayed())
+				{
+					return;
+				}
+				
+			} catch (StaleElementReferenceException e) {
+				return;
+			}
+			
+			Thread.sleep(sleep_interval);
+		}
+		
+		fail(message);
+	}
+	
+	protected void assertNotExistsElement(WebElement element)
+	{
+		assertNotExistsElement(element+" is still exists.", element);
+	}
+	
+	protected void assertNotExistsElement(String message, WebElement element)
+	{
+		try 
+		{
+			element.isDisplayed();
+			fail(message);
+		}
+		catch (StaleElementReferenceException e)
+		{
+			//例外が出なければDOMは存在するので成功
+		}
+	}
+	
+	protected boolean hasClass(WebElement element) {
+		String _class = element.getAttribute("class");
+		
+		String[] part = _class.split(" ");
+		
+		System.out.println(part);
+		
+		return true;
+	}
 }
